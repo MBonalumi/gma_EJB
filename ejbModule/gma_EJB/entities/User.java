322a0814d6773ca32f5eaf5a9e453d3616e2716c
Marco Bonalumi <marco1.bonalumi@mail.polimi.it>
2021-06-10 12:54:44 +0200

    ciao

---
+++ ejbModule/gma_EJB/entities/User.java
@@ -0,0 +1,38 @@
+package gma_EJB.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+@Table(name="user", schema="gma_db")
+//TODO: @NamedQuery
+
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	
+	//attributes
+	@Id
+	int idU;
+	String username;
+	String email;
+	String psw;
+	Boolean ban;
+	
+	//constructor
+	public User() {	}
+	
+	//getters & setters
+	public String getUsername() {		return username;	}
+	public void setUsername(String username) {		this.username = username;	}
+
+	public String getPassword() {		return psw;	}
+	public void setPassword(String psw) {		this.psw = psw;	}
+	
+	public String getEmail() {		return email;	}
+	public void setEmail(String email) {		this.email = email;	}
+
+	public Boolean getAdmin() {		return ban;	}
+	public void setAdmin(Boolean ban) {		this.ban = ban;	}
+	
+}
